7b7540bd03a951299bf0c9ba86637d6b8e13d9cd
Ashik <ashik@pop-os.localdomain>
2023-03-03 20:21:27 +0600

    update head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
+import type { Metadata } from "next";
 import Header from "../components/Header";
 import "../styles/globals.css";
 type LayoutType = {
   children: React.ReactNode;
+};
+
+export const metadata: Metadata = {
+  title: "Medium 2.0",
+  description: "Welcome to Next.js",
 };
 
 export default function RootLayout({ children }: LayoutType) {
